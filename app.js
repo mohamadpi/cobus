3f3cd82b16073dd03287ed237a49a8f2542af534
mohamadhoseinpirasteh <mamad13111383@gmail.com>
2020-03-15 13:47:55 -0700

    finish navbar setting and add some code for main content

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
 var header = document.getElementById("header");
 var textbox = document.getElementById("textbox");
 var mdnav = document.getElementById("mdnav");
+var main = document.getElementById("main");
 
 function dropfunc(){
     if (mdnav.className === "md-nav"){
         mdnav.classList.add ("active");
         textbox.classList.add ("hide");
+        main.style.display = "none"
         
     }else {
         mdnav.classList.remove ("active");
         textbox.classList.remove ("hide"); 
-            
-
-        
-
+        main.style.display = "block"
     } 
 }
 
@@ -25,6 +24,7 @@ window.addEventListener("resize", function() {
     if (window.innerWidth > 680) {
         mdnav.classList.remove ("active");
         textbox.classList.remove ("hide"); 
+        main.style.display = "block"
     };
 });
 
